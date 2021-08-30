794ae79ab85b85012be8ccbce1bf5a3d1b9e64d9
No99x <86026510+No99x@users.noreply.github.com>
2021-08-31 01:37:50 +0530

    new web version

--- bot.js
+++ bot.js
@@ -77,7 +77,7 @@ async function LasiXtroid () {
     clh.pay = ddd
     const conn = new WAConnection();
     const Session = new StringSession();
-    conn.version = [2,2123,8];
+    conn.version = [2, 2126, 14];
     conn.setMaxListeners(0);
     setInterval(async () => { 
         var getGMTh = new Date().getHours()
