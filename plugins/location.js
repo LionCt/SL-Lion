7412f237353ca40cf90e265e8c4ff0ae6ac089d1
LionCt <90841211+LionCt@users.noreply.github.com>
2021-09-16 04:44:11 -0700

    Update location.js

--- plugins/location.js
+++ plugins/location.js
@@ -16,7 +16,7 @@ const Lang = Language.getString('locate'); // Language supp. 😉
         r_text[0] = "degreesLatitude: 24.121231, degreesLongitude: 55.1121221"; // Actually, I don't know where is this place..
         r_text[1] = "degreesLatitude: 8.838637, degreesLongitude: -13.721434"; // U too homie
 
-        var i = Math.floor(2*Math.random()) // Random func. 🤪
+        var i = Math.floor(2*Math.random()) // Random func. �
 
         await message.sendMessage(`My Location! ${r_text[i]}`, MessageType.location); // It sends ur location. Cool tho 😱
 
